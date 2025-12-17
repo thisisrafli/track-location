4e34713f48e308fcb11130c1042eebd9a5de5149
thisisrafli <=>
2025-12-18 06:46:07 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+const input = require("prompt-sync")();
+
+const url = "https://api.ipgeolocation.io/v2/ipgeo";
+const api = "9b2ec68b1def4b9c8a7d33bcfcf1052a";
+
+async function trackLocation(ip) {
+   try {
+      const res = await fetch(`${url}?apiKey=${api}&ip=${ip}`);
+      return await res.json();
+   } catch (e) {
+      return e;
+   }
+}
+
+const ip = input("[+] IP => ");
+
+if (ip === "") {
+   console.log("IP cannot be empty.");
+} else {
+   
+   trackLocation(ip).then(function(results) {
+      console.log(results.location);
+   });
+   
+}
